c0230d6dcb17164260bd0850db40e4d52da5f2eb
Evan Haldane <evanhaldane@gmail.com>
2020-09-17 22:45:17 -0400

    script to check polls and email

---
+++ gmail.py
@@ -0,0 +1,14 @@
+import os
+from yagmail import SMTP
+
+
+def send_message():
+
+    SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+    RECIPIENT_EMAIL = os.getenv("RECIPIENT_EMAIL")
+    yag = SMTP(SENDER_EMAIL, oauth2_file="oauth2_creds.json")
+    body = (
+        "There are new polls available at: https://projects.fivethirtyeight.com/polls/"
+    )
+    yag.send(to=RECIPIENT_EMAIL, subject="New Poll", contents=body)
+
---
+++ script.py
@@ -0,0 +1,27 @@
+import json
+import requests
+from gmail import send_message
+import os.path
+
+with open("polls-initial.json") as f:
+    saved_polls = json.load(f)
+
+saved_president = filter(lambda x: x["type"] == "president-general", saved_polls)
+
+URL = "https://projects.fivethirtyeight.com/polls/polls-initial.json"
+r = requests.get(URL)
+r.raise_for_status()
+polls = r.json()
+president = filter(lambda x: x["type"] == "president-general", polls)
+
+# no timestamp, only ids and ordering
+# assume ordered and just check first item
+new_poll_found = next(president) != next(saved_president)
+
+if new_poll_found:
+    print("sending email...")
+    send_message()
+    print("email sent")
+    with open("polls-initial.json", "w") as f:
+        f.write(r.content)
+        print("saving new polls-initial.json")
