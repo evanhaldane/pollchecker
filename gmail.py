91906bb12bf710751203e888e8ef052274fc0308
evanhaldane <evanhaldane@gmail.com>
2020-09-23 19:41:15 -0400

    add time to notification message

--- gmail.py
+++ gmail.py
@@ -7,8 +7,8 @@ def send_message():
     SENDER_EMAIL = os.getenv("SENDER_EMAIL")
     RECIPIENT_EMAIL = os.getenv("RECIPIENT_EMAIL")
     yag = SMTP(SENDER_EMAIL, oauth2_file="oauth2_creds.json")
-    body = (
-        "There are new polls available at: https://projects.fivethirtyeight.com/polls/"
+    body = "{} There are new polls available at: https://projects.fivethirtyeight.com/polls/".format(
+        datetime.now()
     )
     yag.send(to=RECIPIENT_EMAIL, subject="New Poll", contents=body)
 
