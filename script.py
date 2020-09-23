3fb2e3cab3214828ec0fad68ecb2530fd64de1a1
evanhaldane <evanhaldane@gmail.com>
2020-09-23 19:37:01 -0400

    check for newest id instead of first element

--- script.py
+++ script.py
@@ -6,17 +6,19 @@ import os.path
 with open("polls-initial.json") as f:
     saved_polls = json.load(f)
 
-saved_president = filter(lambda x: x["type"] == "president-general", saved_polls)
+saved_president = [x for x in saved_polls if x["type"] == "president-general"]
 
 URL = "https://projects.fivethirtyeight.com/polls/polls-initial.json"
 r = requests.get(URL)
 r.raise_for_status()
 polls = r.json()
-president = filter(lambda x: x["type"] == "president-general", polls)
+president = [x for x in polls if x["type"] == "president-general"]
 
 # no timestamp, only ids and ordering
-# assume ordered and just check first item
-new_poll_found = next(president) != next(saved_president)
+max_id = max([x["id"] for x in president])
+max_saved_id = max([x["id"] for x in saved_president])
+
+new_poll_found = max_id != max_saved_id
 
 if new_poll_found:
     print("sending email...")
